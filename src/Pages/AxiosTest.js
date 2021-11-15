913da262595c99d4a957f3b04b94912fc1dc3f30
Luda <luda50177@gmail.com>
2021-11-15 17:30:00 +0200

    perPage

--- src/Pages/AxiosTest.js
+++ src/Pages/AxiosTest.js
@@ -5,14 +5,19 @@ import MenuItem from '@mui/material/MenuItem';
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
+import Box from "@mui/material/Box";
 
 class AxiosTest extends Component {
     state = {
         blogList: [],
         pagination: {
             page: 1,
-            perPage: 0,
+            perPage: 10,
             pagesCount: 0,
+        },
+        sort: {
+            field: 'id',
+            orderDesc: false,
         },
     }
 
@@ -23,17 +28,47 @@ class AxiosTest extends Component {
     render() {
         return (
             <List>
-                <FormControl sx={{ minWidth: 150 }}>
-                    <InputLabel id="demo-simple-select-label">Page</InputLabel>
+                <Box sx={{ display: 'flex', justifyContent: "space-between", width: '100%'}}>
+                    <FormControl sx={{ minWidth: 150 }}>
+                        <InputLabel id="select-page">Page</InputLabel>
+                        <Select
+                            value={this.state.pagination.page}
+                            labelId="select-page"
+                            label="Page"
+                            onChange={event => this.changePage(event)}
+                        >
+                            { this.generatePageNumbers().map(pageNo =>
+                                <MenuItem value={pageNo}>{pageNo}</MenuItem>) }
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{ minWidth: 150 }}>
+                        <InputLabel id="select-per-page">PerPage</InputLabel>
+                        <Select
+                            labelId="select-per-page"
+                            value={this.state.pagination.perPage}
+                            label="PerPage"
+                            onChange={event => this.changePerPage(event)}
+                        >
+                            <MenuItem value={10}>10</MenuItem>
+                            <MenuItem value={25}>25</MenuItem>
+                            <MenuItem value={50}>50</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{ minWidth: 300 }}>
+                    <InputLabel id="select-sort">Sort</InputLabel>
                     <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        label="Page"
+                        labelId="select-sort"
+                        value={this.state.sort.field}
+                        label="Sort"
+                        // onChange={}
                     >
-                        { this.generatePageNumbers().map(pageNo =>
-                            <MenuItem value={pageNo} onClick={event => this.changePage(event)}>{pageNo}</MenuItem>) }
+                        <MenuItem value={1}>Id</MenuItem>
+                        <MenuItem value={2}>Date</MenuItem>
+                        <MenuItem value={3}>Title</MenuItem>
+                        <MenuItem value={4}>User</MenuItem>
                     </Select>
                 </FormControl>
+                </Box>
                 <div>page: { this.state.pagination.page }</div>
                 <div>perPage: { this.state.pagination.perPage }</div>
                 <div>pagesCount: { this.state.pagination.pagesCount }</div>
@@ -46,7 +81,13 @@ class AxiosTest extends Component {
     }
 
     getList() {
-        axios.get('http://laravel-blog-test/api/blog?page=' + this.state.pagination.page)
+        const params = new URLSearchParams();
+        params.append('page', this.state.pagination.page);
+        params.append('per_page', this.state.pagination.perPage);
+        params.append('orderBy', 'id');
+        params.append('sortedBy', 'desc'); // asc/desc
+        params.append('with', 'user');
+        axios.get('http://laravel-blog-test/api/blog', {params})
             .then(response => {
                 this.setState({
                     blogList: response.data.data,
@@ -68,13 +109,23 @@ class AxiosTest extends Component {
         return items;
     }
 
+    generateNumberPositions() {
+
+    }
+
     changePage(event) {
-        let pageNo = +(event.target.dataset.value);
+        let pageNo = +(event.target.value);
         this.state.pagination.page = pageNo;
 
         this.getList()
     }
 
+    changePerPage(event) {
+        let numPositions = +(event.target.value);
+        this.state.pagination.perPage = numPositions;
+        this.state.pagination.page = 1;
+        this.getList()
+    }
 }
 
 export default AxiosTest;
