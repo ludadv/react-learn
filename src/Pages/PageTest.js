2d5fd4b7d3c54540b225208460e3eb0f776915de
Luda <luda50177@gmail.com>
2021-11-24 18:57:00 +0200

    pageTest gender

--- src/Pages/PageTest.js
+++ src/Pages/PageTest.js
@@ -5,7 +5,11 @@ import {withStyles} from '@mui/styles';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import styles from "../style";
-import PageTestForm from '../Components/PageTestForm';
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 
 class PageTest extends Component {
@@ -13,28 +17,66 @@ class PageTest extends Component {
         super(props);
         this.state = {
             checked: false,
+            name: '',
+            gender: '',
+            married: false,
         }
     }
     render() {
         const { classes } = this.props;
         return (
             <Box className={classes.container}>
-                <Typography>Имя:{this.props.ree}</Typography>
-                <Typography>Пол:м/ж</Typography>
-                <Typography>Семейное положение: женат/ не женат</Typography>
+                <Typography>Имя:{this.state.name}</Typography>
+                <Typography>Пол:{this.state.gender}</Typography>
+                <Typography>
+                    Семейное положение: {this.state.married ? <div>Женат/Замужем</div> : <div>Холост/Не замужем</div> }
+                </Typography>
                 <FormControlLabel
                     control={<Checkbox checked={this.state.checked}
                     onChange={() => this.changeValue()}/>} label="Редактировать" />
-                {this.state.checked && <PageTestForm />}
+                {this.state.checked &&
+                <Box sx={{margin: '0 auto',  border: '1px solid #000'}}>
+                    <FormControl component="fieldset" variant="standard">
+                        <FormGroup sx={{display: 'flex'}}>
+                            <TextField label="Имя"
+                                       variant="outlined"
+                                       value={this.state.name}
+                                       onChange={event => this.setState({name: event.target.value})}
+                                       sx={{ m: 1, minWidth: 120 }}/>
+                            <Select
+                                value={this.state.gender}
+                                label='Select'
+                                variant="outlined"
+                                onChange={event => this.setState({gender: event.target.value})}
+                            >
+                                <MenuItem value='Мужской'>Мужской</MenuItem>
+                                <MenuItem value='Женский'>Женский</MenuItem>
+                            </Select>
+                            <FormControlLabel
+                                control={<Checkbox checked={this.state.married} onChange={() =>this.changeCheck()}/>}
+                                label='Женат/Замужем' />
+
+
+                        </FormGroup>
+                    </FormControl>
+                </Box>}
 
             </Box>
         );
     }
     changeValue() {
-            this.setState({
-                checked: !this.state.checked,
+        this.setState({
+            checked: !this.state.checked,
         })
     }
+
+    changeCheck() {
+        this.setState({
+            married: !this.state.married,
+            status: 'Женат/Замужем'
+        })
+    }
+
 }
 
 export default withStyles(styles)(PageTest);
